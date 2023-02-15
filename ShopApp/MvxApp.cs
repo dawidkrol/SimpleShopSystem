7c70fbadadf3831f3c6e322b5a3d4e545f229066
dawidkrol7 <dawidkrol7@gmail.com>
2023-02-15 10:27:00 +0100

    Dodaj pliki projektów.

---
+++ ShopApp/MvxApp.cs
@@ -0,0 +1,21 @@
+﻿using Library.Data;
+using MvvmCross;
+using MvvmCross.IoC;
+using MvvmCross.ViewModels;
+using ShopApp.ViewModels;
+
+namespace ShopApp
+{
+    public class MvxApp : MvxApplication
+    {
+        public override void Initialize()
+        {
+            Mvx.IoCProvider.RegisterType<ShopContext>();
+
+            var db = Mvx.IoCProvider.Create<ShopContext>();
+            db.Database.EnsureCreated();
+
+            RegisterAppStart<MenuViewModel>();
+        }
+    }
+}
